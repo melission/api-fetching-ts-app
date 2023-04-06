a3f5012dee011f583f3baf3734e9945475c6ad26
Polina <polina@blsv.space>
2023-04-06 11:35:08 +0200

    eslint --init

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,7 +1,21 @@
-
 module.exports = {
-    extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
-    parser: '@typescript-eslint/parser',
-    plugins: ['@typescript-eslint'],
-    root: true,
-  };
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: [
+    'plugin:react/recommended',
+    'standard-with-typescript'
+  ],
+  overrides: [
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  plugins: [
+    'react'
+  ],
+  rules: {
+  }
+}
