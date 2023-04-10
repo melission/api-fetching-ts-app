e3b4e6df3d89b4e3f1019149897ccdd419f67dbb
Polina <polina@blsv.space>
2023-04-10 17:15:02 +0200

    added loadLikes function

--- src/storage.ts
+++ src/storage.ts
@@ -10,3 +10,11 @@ const LOCAL_STORAGE_KEY = "__REXELS_LIKES__";
 export function saveLikes(likes: StoredUserLikes): void {
     window.localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(likes));
 }
+
+export function loadLikes(): StoredUserLikes | null {
+  const data = window.localStorage.getItem(LOCAL_STORAGE_KEY);
+  if (!data) {
+    return null;
+  }
+  return JSON.parse(data)
+}
