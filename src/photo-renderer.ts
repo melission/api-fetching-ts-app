7ef6d367fed4efeb64634079eed0dae5a4fbdb42
Polina <polina@blsv.space>
2023-04-12 11:32:12 +0200

    added onLikeClick argument to buttn&renderPhoto.

--- src/photo-renderer.ts
+++ src/photo-renderer.ts
@@ -1,8 +1,8 @@
 import { Photo } from "./pexels-api";
 import { html } from "lit-html";
 
-export function renderPhoto(photo: Photo) {
+export function renderPhoto(photo: Photo, onLikeClick: (photoID: number) => void ) {
     return html`<li class="photo"><img src=${photo.src.small} />
-    <button class="like">Like</button>
+    <button class="like" @click=${ () => onLikeClick(photo.id)}>Like</button>
     </li>`
 }
