3b96dd0ae90caa215a6530386519fdaf5ea0934e
Polina <polina@blsv.space>
2023-04-12 10:57:55 +0200

    "like" button was added into renderPhoto function

--- src/photo-renderer.ts
+++ src/photo-renderer.ts
@@ -2,5 +2,7 @@ import { Photo } from "./pexels-api";
 import { html } from "lit-html";
 
 export function renderPhoto(photo: Photo) {
-    return html`<li><img src=${photo.src.small} /></li>`
+    return html`<li class="photo"><img src=${photo.src.small} />
+    <button class="like">Like</button>
+    </li>`
 }
