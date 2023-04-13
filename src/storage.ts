9326a17f5802cfd55c55cc30440641cd2b966acf
Polina <polina@blsv.space>
2023-04-13 14:41:42 +0200

    created: enum LikedResources, type and interface StoredLikes;

--- src/storage.ts
+++ src/storage.ts
@@ -1,17 +1,29 @@
 
-interface StoredUserLikes {
-    photos: number[];
-    videos: number[];
-  }
+// interface StoredUserLikes {
+//     photos: number[];
+//     videos: number[];
+//   }
 
 const LOCAL_STORAGE_KEY = "__REXELS_LIKES__";
 
+export enum LikedResource {
+  Photo = "Photo",
+  Video = "Video"
+};
+
+interface StoredLike {
+  id: number;
+  resourceType: LikedResource;
+}
+
+export type StoredLikes = StoredLike[]
+
 // localStorage stores only strings, so each value needs to be converted. 
-export function saveLikes(likes: StoredUserLikes): void {
+export function saveLikes(likes: StoredLikes): void {
     window.localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(likes));
 }
 
-export function loadLikes(): StoredUserLikes | null {
+export function loadLikes(): StoredLikes | null {
   const data = window.localStorage.getItem(LOCAL_STORAGE_KEY);
   if (!data) {
     return null;
