1d21191dfb3b4ad54fac96ca03ccd821c6cceba5
Polina <polina@blsv.space>
2023-04-13 12:01:30 +0200

    functions renderPhoto & renderVideo are replaced with renderResource; added import Resource; deleted import Photo & Video.

--- src/object-renderer.ts
+++ src/object-renderer.ts
@@ -1,26 +1,40 @@
-import { Photo, Video } from "./pexels-api";
+import { Resource, isPhoto } from "./pexels-api";
 import { html } from "lit-html";
 
-export function renderPhoto(
-    photo: Photo, 
-    onLikeClick: (resource: Photo) => void, 
-    photoIsLiked: boolean 
-    ) {
-    return html`<li class="photo"><img src=${photo.src.small} />
-    <button class="like" @click=${ () => onLikeClick(photo)}>
-    ${photoIsLiked ? "Dislike" : "Like"}
-    </button>
-    </li>`
-}
+// export function renderPhoto(
+//     photo: Photo, 
+//     onLikeClick: (resource: Photo) => void, 
+//     photoIsLiked: boolean 
+//     ) {
+//     return html`<li class="photo"><img src=${photo.src.small} />
+//     <button class="like" @click=${ () => onLikeClick(photo)}>
+//     ${photoIsLiked ? "Dislike" : "Like"}
+//     </button>
+//     </li>`
+// }
 
-export function renderVideo(
-    video: Video, 
-    onLikeClick: (resource: Video) => void,
-    videoIsLiked: boolean
-    ) {
-        return html`<li class="photo"><img src=${video.image} />
-        <button class="like" @click=${() => onLikeClick(video)}>
-        ${videoIsLiked ? "Dislike" : "Like"} 
+// export function renderVideo(
+//     video: Video, 
+//     onLikeClick: (resource: Video) => void,
+//     videoIsLiked: boolean
+//     ) {
+//         return html`<li class="photo"><img src=${video.image} />
+//         <button class="like" @click=${() => onLikeClick(video)}>
+//         ${videoIsLiked ? "Dislike" : "Like"} 
+//         </button>
+//         </li>`
+//     }
+
+export function renderResource(
+    resource: Resource,
+    onLikeClick: (resource: Resource) => void,
+    recourseIsLiked: boolean
+) {
+    const imageURL = isPhoto(resource) ? resource.src.small : resource.image;
+    return html`<li class="photo">
+        <img src=${imageURL} />
+        <button class="like" @click=${() => onLikeClick(resource)}>
+            ${recourseIsLiked ? "Dislike" : "Like"}
         </button>
-        </li>`
-    }
+    </li>`;
+}
